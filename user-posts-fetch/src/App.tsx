cc9fd4376a1565e3a6199da6c130d95d9cde743c
wendiee03 <wmukami03@gmail.com>
2024-09-16 21:37:36 +0300

    Addition of tsconfig file and change to typescript

---
+++ user-posts-fetch/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import UserPosts from './UserPosts.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserPosts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
